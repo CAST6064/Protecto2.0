9143eee7a6f3d73491f75b6f8ba0b209c6bd2184
USUARIO <jcastro5@ibero.edu.co>
2021-01-12 20:33:20 -0500

    Actividad 5

---
+++ carpeta script/script.js
@@ -0,0 +1,14 @@
+function enviar() {
+    return confirm("¿Desea enviar la informacion?");
+     
+ }
+
+ 
+ $(".parrafo").click(function(){
+    $(this).addClass("resaltar0")
+})
+
+$(".parrafo").dblclick(function(){
+    $(this).addClass("resaltar")
+})
+
